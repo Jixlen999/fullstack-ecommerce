bd1888bd8b10a0d6be7e826b083cf680b7ce49fe
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2023-05-19 23:36:58 +0300

    Create navbar and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>vika</div>;
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          {/* <Routes>
+        </Routes> */}
+        </main>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
